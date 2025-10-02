660d0637b136323c876a86f154df831487ad5915
AndyAtmadja03S <atmandy345@gmail.com>
2025-10-02 11:27:54 +1000

    test

--- github-leaderboard/src/server/api/discordbot.ts
+++ github-leaderboard/src/server/api/discordbot.ts
@@ -1,4 +1,5 @@
 'use server';
+import "server-only"; 
 
 import { startLeaderboardJob, stopLeaderboardJob } from './discordScheduler';
 import { registerCommands } from './discordCommands'; // your registration function
